f4099c0d778dd089919468c0158d90608034fa25
kurtag01 <kurt.schumaker@gmail.com>
2020-05-19 16:03:40 -0600

    Update wifidown.py

--- wifidown.py
+++ wifidown.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
 
+import os
+
 location = "/etc/config/wireless"
+location2 = "/etc/config/network"
+
 connect = False
 dns = False
 there = False
 
 #Set the wifi correctly:
-with open (location, "r") as file:
+with open(location, "r") as file:
     wifi = file.readlines()
     for line in wifi:
         if 'wwan' in line:
-            connect = True            
+            connect = True
         if "option disabled '1'" in line:
             there = True
+
+#check to see if dns has already been written
+with open(location2, "r") as file:
+    stuff = file.readlines()
+    for line in stuff:
         if '209.222.18.218 209.222.18.222' in line:
             dns = True
-                              
+            
+#now we write the files
 if connect == True and there == False:
     with open(location, "a") as file:
         file.writelines("\toption disabled '1'")
-      
+
 if dns == False and connect == True:
-    with open(location, "a") as file:
-        file.writelines("\n\toption dns '209.222.18.218 209.222.18.222'")    
+    with open(location2, "r+") as old_file, open("/etc/config/network.new", "w") as new_file:
+        for line in old_file:
+            new_file.write(line)
+            if 'dhcp' in line:
+                new_file.write("\tlist dns '209.222.18.218'\n\tlist dns '209.222.18.222'\n\toption peerdns '0'\n")
+
+os.rename("/etc/config/network.new", location2)
+
